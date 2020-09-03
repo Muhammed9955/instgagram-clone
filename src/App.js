15696d2f32b7246c3650d00e6bd2b2674ce55d91
Muhammed khaled <muhammmed.k95@gmail.com>
2020-09-03 14:36:37 +0200

    instagram clone added

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function getModalStyle() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
-    width: 400,
+    width: "60vw",
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    // padding: theme.spacing(2, 4, 3),
   },
 }));
 
